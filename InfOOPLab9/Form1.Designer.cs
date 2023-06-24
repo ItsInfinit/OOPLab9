3816d0b7956d2c45cd5af9a05ccc0bcb066b0e55
Inf.init <www.mgv29@gmail.com>
2023-06-24 21:02:03 +0300

    Add project files.

---
+++ InfOOPLab9/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace InfOOPLab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphicDraw = new System.Windows.Forms.PictureBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.rTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.graphicDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphicDraw
+            // 
+            this.graphicDraw.Location = new System.Drawing.Point(12, 12);
+            this.graphicDraw.Name = "graphicDraw";
+            this.graphicDraw.Size = new System.Drawing.Size(408, 284);
+            this.graphicDraw.TabIndex = 0;
+            this.graphicDraw.TabStop = false;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDraw.Location = new System.Drawing.Point(272, 307);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(148, 35);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // rTextBox
+            // 
+            this.rTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rTextBox.Location = new System.Drawing.Point(36, 309);
+            this.rTextBox.Name = "rTextBox";
+            this.rTextBox.Size = new System.Drawing.Size(100, 30);
+            this.rTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "r";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(142, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "h";
+            // 
+            // hTextBox
+            // 
+            this.hTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hTextBox.Location = new System.Drawing.Point(166, 309);
+            this.hTextBox.Name = "hTextBox";
+            this.hTextBox.Size = new System.Drawing.Size(100, 30);
+            this.hTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 355);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rTextBox);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.graphicDraw);
+            this.Name = "Form1";
+            this.Text = "Lab9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.graphicDraw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphicDraw;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TextBox rTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox hTextBox;
+    }
+}
+
---
+++ InfOOPLab9/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace InfOOPLab9
+{
+    public partial class Form1 : Form
+    {
+        private float r;
+        private float h;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            r = (float)double.Parse(rTextBox.Text);
+            h = (float)double.Parse(hTextBox.Text);
+
+            Bitmap bitmap = new Bitmap(graphicDraw.Width, graphicDraw.Height);
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.White);
+
+                double scaleX = graphicDraw.Width / 20.0;
+                double scaleY = graphicDraw.Height / 20.0;
+
+                DrawCoordinateSystem(graphics, scaleX, scaleY);
+
+                DrawFunction(graphics, scaleX, scaleY);
+            }
+
+            graphicDraw.Image = bitmap;
+        }
+
+        private void DrawCoordinateSystem(Graphics graphics, double scaleX, double scaleY)
+        {
+            graphics.DrawLine(Pens.Black, 0, graphicDraw.Height / 2, graphicDraw.Width, graphicDraw.Height / 2);
+            graphics.DrawLine(Pens.Black, graphicDraw.Width / 2, 0, graphicDraw.Width / 2, graphicDraw.Height);
+
+            graphics.DrawString("X", DefaultFont, Brushes.Black, graphicDraw.Width - 20, graphicDraw.Height / 2 - 15);
+            graphics.DrawString("Y", DefaultFont, Brushes.Black, graphicDraw.Width / 2 + 5, 5);
+
+            for (int i = -10; i <= 10; i++)
+            {
+                int x = graphicDraw.Width / 2 + (int)(i * scaleX);
+                graphics.DrawString(i.ToString(), DefaultFont, Brushes.Black, x, graphicDraw.Height / 2 + 5);
+            }
+
+            for (int i = -10; i <= 10; i++)
+            {
+                int y = graphicDraw.Height / 2 - (int)(i * scaleY);
+                graphics.DrawString(i.ToString(), DefaultFont, Brushes.Black, graphicDraw.Width / 2 - 15, y);
+            }
+        }
+
+        private void DrawFunction(Graphics graphics, double scaleX, double scaleY)
+        {
+            int numPoints = 100;
+
+            double tMin = 0;
+            double tMax = 2 * Math.PI;
+
+            double xPrev = r * tMin - h * Math.Sin(tMin);
+            double yPrev = r - h * Math.Cos(tMin);
+            int pixelXPrev = graphicDraw.Width / 2 + (int)(xPrev * scaleX);
+            int pixelYPrev = graphicDraw.Height / 2 - (int)(yPrev * scaleY);
+
+            for (int i = 1; i <= numPoints; i++)
+            {
+                double t = tMin + (i / (double)numPoints) * (tMax - tMin);
+
+                double x = r * t - h * Math.Sin(t);
+                double y = r - h * Math.Cos(t);
+
+                int pixelX = graphicDraw.Width / 2 + (int)(x * scaleX);
+                int pixelY = graphicDraw.Height / 2 - (int)(y * scaleY);
+
+                graphics.DrawLine(Pens.Red, pixelXPrev, pixelYPrev, pixelX, pixelY);
+
+                xPrev = x;
+                yPrev = y;
+                pixelXPrev = pixelX;
+                pixelYPrev = pixelY;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
